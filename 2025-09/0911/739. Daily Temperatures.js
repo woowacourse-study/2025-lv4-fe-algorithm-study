b48ac0d3009e788cfed3e1c562daf5ff935427c8
Ko Daeun <drechsu@gmail.com>
2025-09-11 16:07:45 +0900

    09.11 Daily Temperatures - stack

---
+++ 2025-09/0911/739. Daily Temperatures.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+
+var dailyTemperatures = function (temperatures) {
+  const result = new Array(temperatures.length).fill(0);
+  const stack = [];
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const idx = stack.pop();
+      result[idx] = i - idx;
+    }
+    stack.push(i);
+  }
+
+  return result;
+};
