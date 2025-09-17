ed4a2f19a6fe3441b99ae476b195051062f4e73d
장정안 <wjddks96@naver.com>
2025-09-17 22:18:11 +0900

    0917 백준 블랙잭

---
+++ 2025-09/0917/0917.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const stdin = fs
+  .readFileSync('input.txt')
+  .toString()
+  .split('\n')
+  .map((num) => num.split(' ').map(Number));
+
+function solution() {
+  const [cardNum, maximum] = stdin[0];
+  const [...numbers] = stdin[1];
+  let answers = 0;
+  for (let i = 0; i < cardNum; i++) {
+    for (let j = i + 1; j < cardNum; j++) {
+      for (let k = i + 2; k < cardNum; k++) {
+        const sum = numbers[i] + numbers[j] + numbers[k];
+        const gap = maximum - sum;
+        if (gap >= 0 && answers <= sum) {
+          answers = sum;
+        }
+      }
+    }
+  }
+  console.log(answers);
+}
+
+solution();
