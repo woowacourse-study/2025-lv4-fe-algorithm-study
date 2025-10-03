a8ade3783b05bc4bdc6965a44a9deb06acfad422
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-03 23:04:51 +0900

    k진수에서소수개수구하기: 구현

---
+++ 2025-09/1003/k진수에서소수개수구하기.js
@@ -0,0 +1,39 @@
+function convert(n, k) {
+  let num = n
+  let rest = n % k
+  let converted = rest.toString()
+  while (num > k) {
+    num = Math.floor(num / k)
+    rest = num % k
+    converted = rest.toString() + converted
+  }
+
+  return converted
+}
+
+function isPrime(num) {
+  if (num === 1) {
+    return false
+  }
+  if (num === 2 || num === 3) {
+    return true
+  }
+  const end = Math.floor(Math.sqrt(num)) + 1
+
+  for (let i = 2; i < end; i += 1) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+function solution(n, k) {
+  var answer = -1
+  const convertedNum = convert(n, k)
+
+  const splittedNums = convertedNum.split("0").filter(Boolean).map(Number)
+
+  answer = splittedNums.filter((splittedNum) => isPrime(splittedNum)).length
+  return answer
+}
