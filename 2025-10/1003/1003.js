7f2723d733af0d5f21828a7d36b989483a7bc51a
장정안 <wjddks96@naver.com>
2025-10-03 23:05:20 +0900

    1003 백준 오늘 날짜

---
+++ 2025-10/1003/1003.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = (today.getMonth() + 1).toString().padStart(2, '0');
+const day = today.getDate().toString().padStart(2, '0');
+
+const answer = `${year}-${month}-${day}`;
+console.log(answer);
