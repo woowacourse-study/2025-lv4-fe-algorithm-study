be6f53602c79732d06390a04106ecc6a092b8f25
장정안 <wjddks96@naver.com>
2025-11-12 16:36:38 +0900

    삼각형판별하기

---
+++ 2025-11/1111/삼각형판별하기.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const stdin = fs
+  .readFileSync('input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+function solution() {
+  //가장 긴 변의 길이가 나머지 두 변의 길이의 합보다 작아야 함.
+  //1. 세 수 중에 최댓값을 찾아.
+  const max = Math.max(...stdin);
+  console.log(max);
+  //2. max를 제외한 나머지 두 수는?
+  const maxExcept = stdin
+    .filter((num) => num !== max)
+    .reduce((acc, cur) => acc + cur, 0);
+  console.log(maxExcept);
+  if (max < maxExcept) console.log('YES');
+  else console.log('NO');
+}
+
+solution();
