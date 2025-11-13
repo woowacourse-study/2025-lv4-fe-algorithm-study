451327d9c997ad44221c027af6ff9e81f68d7f11
장정안 <wjddks96@naver.com>
2025-11-13 16:26:36 +0900

    세수중최솟값 3차풀이

---
+++ 2025-11/1113/세수중최솟값3.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const stdin = fs
+  .readFileSync('input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+function solution() {
+  const [a, b, c] = stdin;
+  let answer;
+  if (a < b) {
+    answer = a;
+  } else {
+    answer = b;
+  }
+  if (c < answer) {
+    answer = c;
+  }
+  console.log(answer);
+}
+
+solution();
